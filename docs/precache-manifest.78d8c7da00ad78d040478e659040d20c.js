014d02a02a38ca621907aa79dbba9a31fe318dc4
JavascriptDeNoobAPro <javascriptdenoobapro@gmail.com>
2022-02-07 11:38:19 +0100

    docs added

---
+++ docs/precache-manifest.78d8c7da00ad78d040478e659040d20c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5430c96592e8d1f20896e59bd2b19884",
+    "url": "/index.html"
+  },
+  {
+    "revision": "223631344b70dc4a2a5f",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "223631344b70dc4a2a5f",
+    "url": "/static/js/main.da225fff.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
